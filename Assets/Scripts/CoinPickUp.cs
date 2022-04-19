792860233a88779f0dfd74936799dcf52692ca94
Schustlo <90356719+Schustlo@users.noreply.github.com>
2022-04-18 22:40:22 -0400

    Added UI for coins

---
+++ Assets/Scripts/CoinPickUp.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinPickUp : MonoBehaviour
+{
+    public int coins;
+    Text text;
+    public static int coinAmount;
+
+    void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        text.text = coinAmount.ToString();
+    }
+
+    public void OnTriggerEnter(Collider Col)
+    {
+        if (Col.gameObject.tag == "Coin")
+
+        {
+            Debug.Log("Coin collected!");
+            coins = coins + 1;
+            //Col.gameObject.SetActive(false);
+            Destroy(Col.gameObject);
+        }
+    }
+}
